a8a06c4fbdaa7e3346ea4399813df01b33a34c7f
Laila Ainur Rahma <111741203+laainra@users.noreply.github.com>
2023-12-17 16:28:37 +0700

    aes up

--- AES/aes.py
+++ AES/aes.py
@@ -1,21 +1,39 @@
+# Mengimpor modul AES dari pustaka Crypto
 from Crypto.Cipher import AES
+
+# Mengimpor fungsi get_random_bytes dari modul Crypto.Random
 from Crypto.Random import get_random_bytes
 
+# Menghasilkan kunci acak sepanjang 16 byte
 key = get_random_bytes(16)
-data_to_encrypt = input("Masukkan teks : ")
 
+# Memasukkan data yang akan dienkripsi
+data_to_encrypt = input("Masukkan teks: ")
+
+# Mengonversi data menjadi byte dengan encoding utf-8
 data = data_to_encrypt.encode('utf-8')
 
+# Membuat objek kriptografi AES untuk enkripsi dalam mode CFB
 cipher_encrypt = AES.new(key, AES.MODE_CFB)
+
+# Melakukan enkripsi data
 ciphered_bytes = cipher_encrypt.encrypt(data)
 
+# Mendapatkan vektor inisialisasi (IV) yang digunakan dalam enkripsi
 iv = cipher_encrypt.iv
+
+# Menyimpan hasil enkripsi dan IV
 ciphered_data = ciphered_bytes
 
+# Membuat objek kriptografi AES untuk dekripsi dalam mode CFB dengan IV yang sama
 cipher_decrypt = AES.new(key, AES.MODE_CFB, iv=iv)
+
+# Melakukan dekripsi data
 deciphered_bytes = cipher_decrypt.decrypt(ciphered_data)
 
+# Mengonversi byte hasil dekripsi menjadi string dengan encoding utf-8
 decrypted_data = deciphered_bytes.decode('utf-8')
 
-print("hasil enkripsi : " , ciphered_data)
-print("hasil dekripsi : " ,decrypted_data)
+# Menampilkan hasil enkripsi dan dekripsi
+print("Hasil Enkripsi: ", ciphered_data)
+print("Hasil Dekripsi: ", decrypted_data)
